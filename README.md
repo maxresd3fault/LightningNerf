e258e9ed6e411652f3d1de64d6843f8fbe23f595
maxresd3fault <64054698+maxresd3fault@users.noreply.github.com>
2025-05-06 17:26:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# LightningNerf
+Have you ever been paranoid of lightning strikes burning down forests and/or builds in your b1.7.3 server? It is rare, but possible, if the fire has the chance to spread to a block covered from the rain. On 24/7 servers this becomes more likely to happen eventually.
+
+This plugin simply cancels all fires caused by lighting, otherwise lightning behaves in the same way.
+
+## Config Options
+
+- `quiet-mode: false` - By default the plugin will announce when it cancels a lightning fire and provides the coordinates. During thunderstorms this can become quite spammy in the server log and some lightning strikes might cause multiple fires on the same block. This option disables these log messages.
---
+++ src/maxresdefault/LightningNerf.java
@@ -0,0 +1,64 @@
+package maxresdefault;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+public class LightningNerf extends JavaPlugin {
+	
+	public static final String VERSION = "R01";
+	private Configuration cfg;
+	public boolean quietMode;
+	
+	@Override
+	public void onEnable() {
+		
+		if (!getDataFolder().exists()) {
+	    	getDataFolder().mkdirs();
+	    }
+		File configFile = new File(getDataFolder(), "config.yml");
+		cfg = new Configuration(configFile);
+		if (!configFile.exists()) {
+			cfg.setProperty("quiet-mode", false);
+			cfg.save();
+		} else {
+			cfg.load();
+		}
+		quietMode = cfg.getBoolean("quiet-mode", false);
+		
+    	getServer().getPluginManager().registerEvent(Event.Type.BLOCK_IGNITE, new MyBlockListener(), Event.Priority.Normal, this);
+    	
+    	if(quietMode) {
+    		log("Quiet mode enabled - plugin will not announce blocked lightning fires");
+    	}
+    	log("Enabled");
+	}
+	
+	@Override
+	public void onDisable() {
+    	log("Disabled");
+    }
+	
+	public void log(String s) {
+		Logger.getLogger("Minecraft").info("[LightningNerf " + VERSION + "]: " + s);
+	}
+	
+	public class MyBlockListener extends BlockListener {
+		@Override
+		public void onBlockIgnite(BlockIgniteEvent event) {
+			if(event.getCause() == BlockIgniteEvent.IgniteCause.LIGHTNING) {
+				event.setCancelled(true);
+				Block block = event.getBlock();
+				if(!quietMode) {
+					log("Stopped lightning fire at x=" + block.getLocation().getBlockX() + ", y=" + block.getLocation().getBlockY() + ", z=" + block.getLocation().getBlockZ());
+				}
+			}
+		}
+	}
+}
